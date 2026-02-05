3ac897dc6a1316c63b26b2a790567ef68e4ed25c
PaoloMucedero <p.mucedero@gmail.com>
2026-02-05 19:25:21 +0100

    Testata creazione oggetto JS

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const app = express()
 const port = 3000
 
 app.get(`/`, (req, res) => {
-    res.send(`Server del mio blog!`)
+    // Creo l'oggetto JS che rappresenta la lista post
+    const articleList = {
+        articleTitle: "test"
+
+    }
+
+    res.json(articleList);
 })
 
 app.listen(port, () => {
