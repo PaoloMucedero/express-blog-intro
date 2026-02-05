62b7f1accf07701552f0a8ca1927df628028e34f
PaoloMucedero <p.mucedero@gmail.com>
2026-02-05 20:53:45 +0100

    aggiunta route principale e route /bacheca. Testato funzionamento

--- app.js
+++ app.js
@@ -2,13 +2,54 @@ const express = require(`express`)
 const app = express()
 const port = 3000
 
-app.get(`/`, (req, res) => {
+// attivazione cartella public
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    res.send("<h1>Home Route app Blog</h1>")
+})
+
+app.get("/bacheca", (req, res) => {
     // Creo l'oggetto JS che rappresenta la lista post
-    const articleList = {
-        articleTitle: "test"
-
-    }
-
+    const articleList = [
+        {   
+            id: 1,
+            articleTitle: "test",
+            articleBody: "Test corpo articolo",
+            img: "/images/ciambellone.jpeg",
+            tags: ["", ""]
+        },
+       {    
+            id: 2,
+            articleTitle: "test",
+            articleBody: "Test corpo articolo",
+            img: "/images/cracker_barbabietola.jpeg",
+            tags: ["", ""]
+        },
+        {   
+            id: 3,
+            articleTitle: "test",
+            articleBody: "Test corpo articolo",
+            img: "/images/pane_fritto_dolce.jpeg",
+            tags: ["", ""]
+        },
+        {   
+            id: 4,
+            articleTitle: "test",
+            articleBody: "Test corpo articolo",
+            img: "/images/pasta_barbabietola.jpeg",
+            tags: ["", ""]
+        },
+        {   
+            id: 5,
+            articleTitle: "test",
+            articleBody: "Test corpo articolo",
+            img: "/images/torta_paesana.jpeg",
+            tags: ["", ""]
+        },
+]
+    
     res.json(articleList);
 })
 
